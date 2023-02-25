ca468c19a57389f4c9c3182a02c8a19db6ba8375
František Kmječ <frantisek.kmjec@gmail.com>
2023-02-25 09:15:53 +0100

    Fix incorrect octave numbers

--- tone_utils.cpp
+++ tone_utils.cpp
@@ -8,9 +8,8 @@ const std::vector<std::string> NOTES = {"A","A#","B","C","C#","D","D#","E","F","
 
 std::pair<std::string, double> getClosestPitch(double pitch, double concertPitch) {
     int i = nearbyint(log2(pitch / concertPitch) * 12);
-    // TODO: study how this works, I got this from the python tutorial
-    std::string octaveNum = std::to_string(4 + (i + 9) / 12 - 1);
-    int index = (i % 12 + 12) % 12; // FIXME
+    std::string octaveNum = std::to_string(4 + (int)floor((i + 9) / 12.0));
+    int index = (i % 12 + 12) % 12;
     std::string closestNote = NOTES[index] + octaveNum;
     double closestPitch = concertPitch * pow(2, i / 12.0);
     return std::make_pair(closestNote, closestPitch); 
